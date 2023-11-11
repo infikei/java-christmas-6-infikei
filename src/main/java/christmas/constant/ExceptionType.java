84010a7c863b1b96bb458a56fdb6267e48fa6cbf
Infikei <hckhong@naver.com>
2023-11-11 20:58:37 +0900

    feat(ExceptionType): 예외 종류에 대한 상수 추가

---
+++ src/main/java/christmas/constant/ExceptionType.java
@@ -0,0 +1,18 @@
+package christmas.constant;
+
+public enum ExceptionType {
+    INVALID_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_ORDERS("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+    private static final String MESSAGE_PREFIX = "[ERROR] ";
+
+    private final String message;
+
+    private ExceptionType(String message) {
+        this.message = MESSAGE_PREFIX + message;
+    }
+
+    public void throwException() {
+        throw new IllegalArgumentException(message);
+    }
+}
