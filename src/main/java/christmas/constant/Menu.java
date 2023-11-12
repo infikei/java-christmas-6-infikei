645a939fbc0cd518f00b764af40ce172f1d3d2ac
Infikei <hckhong@naver.com>
2023-11-12 19:03:49 +0900

    feat(Menu): 메뉴에 대한 상수 추가

---
+++ src/main/java/christmas/constant/Menu.java
@@ -0,0 +1,32 @@
+package christmas.constant;
+
+public enum Menu {
+    MUSHROOM_SOUP("양송이수프", 6_000),
+    TAPAS("타파스", 5_500),
+    CAESAR_SALAD("시저샐러드", 8_000),
+    T_BONE_STEAK("티본스테이크", 55_000),
+    BBQ_RIBS("바비큐립", 54_000),
+    SEAFOOD_PASTA("해산물파스타", 35_000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000),
+    CHOCO_CAKE("초코케이크", 15_000),
+    ICECREAM("아이스크림", 5_000),
+    ZERO_COKE("제로콜라", 3_000),
+    RED_WINE("레드와인", 60_000),
+    CHAMPAGNE("샴페인", 25_000);
+
+    private final String name;
+    private final int price;
+
+    private Menu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
