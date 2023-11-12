8e6c35f5a5c04ca3e5786344284a3974a295b5e0
Infikei <hckhong@naver.com>
2023-11-13 02:17:00 +0900

    feat(OutputView): 플래너 시작 메시지 출력 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,9 @@
+package christmas.view;
+
+public class OutputView {
+    private static final String PRINT_PLANNER_START = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public void printPlannerStart() {
+        System.out.println(PRINT_PLANNER_START);
+    }
+}
