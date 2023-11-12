1e18d4d6fa2b1bf2da71b1c9439b4db4a8a917e7
Infikei <hckhong@naver.com>
2023-11-13 02:32:58 +0900

    refactor: DateValidator -> Date로 변경
    - validator가 아닌 model의 형식으로 변경

--- src/main/java/christmas/model/Date.java
+++ src/main/java/christmas/model/Date.java
@@ -1,12 +1,19 @@
-package christmas.validator;
+package christmas.model;
 
 import christmas.constant.ExceptionType;
 
-public class DateValidator {
+public class Date {
     private static final int DECEMBER_DATE_BEGIN = 1;
     private static final int DECEMBER_DATE_END = 31;
 
-    public void validate(int date) {
+    private final int date;
+
+    public Date(int date) {
+        validate(date);
+        this.date = date;
+    }
+
+    private void validate(int date) {
         if (!isDecemberDate(date)) {
             throw new IllegalArgumentException(ExceptionType.INVALID_DATE.getMessage());
         }
@@ -14,5 +21,9 @@ public class DateValidator {
 
     private boolean isDecemberDate(int date) {
         return (date >= DECEMBER_DATE_BEGIN) && (date <= DECEMBER_DATE_END);
+    }
+
+    public int getDate() {
+        return date;
     }
 }
