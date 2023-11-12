8fa59b9fe8f3667b6e882dfd3a2ebf2787ee57fe
Infikei <hckhong@naver.com>
2023-11-12 18:13:21 +0900

    feat(TypeConverter): 문자열을 구분자로 구분하는 기능 구현

--- src/main/java/christmas/util/TypeConverter.java
+++ src/main/java/christmas/util/TypeConverter.java
@@ -1,7 +1,22 @@
 package christmas.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TypeConverter {
     public static int toInteger(String number) throws NumberFormatException {
         return Integer.parseInt(number);
+    }
+
+    public static List<String> split(String values, char separator) {
+        if (values.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (values.charAt(values.length() - 1) == separator) {
+            throw new IllegalArgumentException();
+        }
+
+        return new ArrayList<>(Arrays.asList(values.split(String.valueOf(separator))));
     }
 }
