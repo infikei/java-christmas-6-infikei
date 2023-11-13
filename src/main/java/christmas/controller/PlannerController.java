ef9a20424803e52b4ffd0123c19e93ed4e0c0da3
Infikei <hckhong@naver.com>
2023-11-14 05:15:48 +0900

    feat(PlannerController): PlannerController 클래스 생성
    - 방문 날짜 입력 후 저장 기능 구현
    - 주문 메뉴와 개수 입력 후 저장 기능 구현

---
+++ src/main/java/christmas/controller/PlannerController.java
@@ -0,0 +1,59 @@
+package christmas.controller;
+
+import christmas.model.Date;
+import christmas.model.Orders;
+import christmas.service.PlannerService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class PlannerController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final PlannerService plannerService;
+
+    public PlannerController() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        plannerService = new PlannerService();
+    }
+
+    public void start() {
+        generatePlanner();
+        generatePlannerResult();
+        printPlannerResult();
+    }
+
+    private void generatePlanner() {
+        outputView.printPlannerStart();
+        plannerService.saveDate(readDate());
+        plannerService.saveOrders(readOrders());
+    }
+
+    private Date readDate() {
+        while (true) {
+            try {
+                return new Date(inputView.readDate());
+            } catch (IllegalArgumentException e) {
+                outputView.printExceptionMessage(e.getMessage());
+            }
+        }
+    }
+
+    private Orders readOrders() {
+        while (true) {
+            try {
+                return new Orders(inputView.readOrders());
+            } catch (IllegalArgumentException e) {
+                outputView.printExceptionMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void generatePlannerResult() {
+        //
+    }
+
+    private void printPlannerResult() {
+        //
+    }
+}
