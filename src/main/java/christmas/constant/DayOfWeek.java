373fd9106f0125b9b9033c42ff2d97711e59f035
Infikei <hckhong@naver.com>
2023-11-14 03:28:54 +0900

    feat(DayOfWeek): 요일에 대한 상수 추가
    - 12월의 날짜을 입력받으면 해당하는 요일을 반환하는 기능 구현

---
+++ src/main/java/christmas/constant/DayOfWeek.java
@@ -0,0 +1,26 @@
+package christmas.constant;
+
+public enum DayOfWeek {
+    SUNDAY(3),
+    MONDAY(4),
+    TUESDAY(5),
+    WEDNESDAY(6),
+    THURSDAY(0),
+    FRIDAY(1),
+    SATURDAY(2);
+
+    private final int remainderOfDecemberDate;
+
+    private DayOfWeek(int remainderOfDecemberDate) {
+        this.remainderOfDecemberDate = remainderOfDecemberDate;
+    }
+
+    public static DayOfWeek dateOf(int decemberDate) {
+        for (DayOfWeek dayOfWeek : values()) {
+            if (dayOfWeek.remainderOfDecemberDate == decemberDate % 7) {
+                return dayOfWeek;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
