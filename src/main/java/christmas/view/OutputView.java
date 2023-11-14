19c26b5eb74988182b409884da6c2ec04723ce45
Infikei <hckhong@naver.com>
2023-11-14 19:29:13 +0900

    feat(OutputView): 할인 전 총주문 금액 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,6 +8,8 @@ public class OutputView {
     private static final String PRINT_PLANNER_START = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String PRINT_RESULT_START_FORMAT = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String PRINT_ORDERS_START = "<주문 메뉴>";
+    private static final String PRINT_ORDERS_PRICE_SUM_START = "<할인 전 총주문 금액>";
+    private static final String PRINT_ORDERS_PRICE_SUM_FORMAT = "%,d원";
 
     public void printPlannerStart() {
         System.out.println(PRINT_PLANNER_START);
@@ -23,6 +25,11 @@ public class OutputView {
         for (Order order : orders) {
             System.out.println(order);
         }
+    }
+
+    public void printOrdersPriceSum(int ordersPriceSum) {
+        System.out.println(PRINT_ORDERS_PRICE_SUM_START);
+        System.out.println(String.format(PRINT_ORDERS_PRICE_SUM_FORMAT, ordersPriceSum));
     }
 
     public void printExceptionMessage(String message) {
