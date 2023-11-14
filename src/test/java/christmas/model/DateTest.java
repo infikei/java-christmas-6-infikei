6a7168560fa62ec14803da926ccb0abdec2470d3
Infikei <hckhong@naver.com>
2023-11-14 20:33:37 +0900

    test(Date): 날짜 기능 테스트 구현

---
+++ src/test/java/christmas/model/DateTest.java
@@ -0,0 +1,36 @@
+package christmas.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class DateTest {
+    @Test
+    void 날짜가_0인_경우_예외_발생() {
+        assertThatThrownBy(() -> new Date(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 날짜가_음수인_경우_예외_발생() {
+        assertThatThrownBy(() -> new Date(-1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 날짜가_32인_경우_예외_발생() {
+        assertThatThrownBy(() -> new Date(32))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 날짜가_1인_경우_결과_확인() {
+        assertThat(new Date(1).getDate()).isEqualTo(1);
+    }
+
+    @Test
+    void 날짜가_31인_경우_결과_확인() {
+        assertThat(new Date(31).getDate()).isEqualTo(31);
+    }
+}
