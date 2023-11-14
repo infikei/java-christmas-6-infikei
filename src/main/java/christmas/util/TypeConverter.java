b8ecb47ee4c03d5dc92d4be5204ca0cfc23c224f
Infikei <hckhong@naver.com>
2023-11-15 02:45:12 +0900

    refactor(InputView,TypeConverter): 타입 변환 기능 이동
    - InputView에서 타입 변환 기능을 담당하지 않도록 TypeConverter로 이동
    - 타입 변환 로직 개선

--- src/main/java/christmas/util/TypeConverter.java
+++ src/main/java/christmas/util/TypeConverter.java
@@ -2,7 +2,9 @@ package christmas.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TypeConverter {
     public static int toInteger(String number) throws NumberFormatException {
@@ -17,5 +19,63 @@ public class TypeConverter {
             throw new IllegalArgumentException();
         }
         return new ArrayList<>(Arrays.asList(values.split(String.valueOf(separator))));
+    }
+
+    public static List<List<String>> splitExactlyDouble(String values, char separator, char innerSeparator) {
+        List<List<String>> valuesConverted = new ArrayList<>();
+
+        for (String value : splitExactly(values, separator)) {
+            valuesConverted.add(splitExactly(value, innerSeparator));
+        }
+        return valuesConverted;
+    }
+
+    public static Map<String, Integer> toMapOfIntegerValue(String values, char separator, char innerSeparator) {
+        return toMapOfIntegerValue(toMap(values, separator, innerSeparator));
+    }
+
+    public static Map<String, Integer> toMapOfIntegerValue(Map<String, String> pairs) {
+        Map<String, Integer> pairsConverted = new HashMap<>();
+
+        for (Map.Entry<String, String> pair : pairs.entrySet()) {
+            pairsConverted.put(pair.getKey(), toInteger(pair.getValue()));
+        }
+        return pairsConverted;
+    }
+
+    public static Map<String, String> toMap(String pairs, char separator, char innerSeparator) {
+        return toMap(splitExactlyDouble(pairs, separator, innerSeparator));
+    }
+
+    public static Map<String, String> toMap(List<List<String>> pairs) {
+        Map<String, String> pairsConverted = new HashMap<>();
+
+        for (List<String> pair : pairs) {
+            validateIsSizeTwo(pair);
+            pairsConverted.put(pair.get(0), pair.get(1));
+        }
+
+        validateIsSizeSame(pairs, pairsConverted);
+        return pairsConverted;
+    }
+
+    private static void validateIsSizeTwo(List<String> values) {
+        if (!isSizeTwo(values)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isSizeTwo(List<String> values) {
+        return values.size() == 2;
+    }
+
+    private static void validateIsSizeSame(List<List<String>> value, Map<String, String> other) {
+        if (!isSizeSame(value, other)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isSizeSame(List<List<String>> value, Map<String, String> other) {
+        return value.size() == other.size();
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,9 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.constant.ExceptionType;
 import christmas.util.TypeConverter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class InputView {
@@ -27,33 +24,9 @@ public class InputView {
         System.out.println(INPUT_ORDERS);
 
         try {
-            return convertReadOrders(splitReadOrders(Console.readLine()));
+            return TypeConverter.toMapOfIntegerValue(Console.readLine(), ',', '-');
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(ExceptionType.INVALID_ORDERS.getMessage());
         }
-    }
-
-    private List<List<String>> splitReadOrders(String orders) {
-        List<List<String>> ordersConverted = new ArrayList<>();
-
-        for (String order : TypeConverter.splitExactly(orders, ',')) {
-            ordersConverted.add(TypeConverter.splitExactly(order, '-'));
-        }
-        return ordersConverted;
-    }
-
-    private Map<String, Integer> convertReadOrders(List<List<String>> orders) {
-        Map<String, Integer> ordersConverted = new HashMap<>();
-
-        for (List<String> order : orders) {
-            if (order.size() != 2) {
-                throw new IllegalArgumentException();
-            }
-            if (ordersConverted.containsKey(order.get(0))) {
-                throw new IllegalArgumentException();
-            }
-            ordersConverted.put(order.get(0), TypeConverter.toInteger(order.get(1)));
-        }
-        return ordersConverted;
     }
 }
