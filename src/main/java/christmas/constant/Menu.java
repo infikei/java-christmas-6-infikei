f8e89fd0d937d357ce71cc1c991b57504e2bfc70
Infikei <hckhong@naver.com>
2023-11-14 21:31:20 +0900

    refactor(Menu): 생성자가 문자열이 아닌 MenuCategory 상수를... ...입력인자로 전달받도록 변경

--- src/main/java/christmas/constant/Menu.java
+++ src/main/java/christmas/constant/Menu.java
@@ -1,27 +1,27 @@
 package christmas.constant;
 
 public enum Menu {
-    MUSHROOM_SOUP("양송이수프", 6_000, "애피타이저"),
-    TAPAS("타파스", 5_500, "애피타이저"),
-    CAESAR_SALAD("시저샐러드", 8_000, "애피타이저"),
-    T_BONE_STEAK("티본스테이크", 55_000, "메인"),
-    BBQ_RIBS("바비큐립", 54_000, "메인"),
-    SEAFOOD_PASTA("해산물파스타", 35_000, "메인"),
-    CHRISTMAS_PASTA("크리스마스파스타", 25_000, "메인"),
-    CHOCO_CAKE("초코케이크", 15_000, "디저트"),
-    ICECREAM("아이스크림", 5_000, "디저트"),
-    ZERO_COKE("제로콜라", 3_000, "음료"),
-    RED_WINE("레드와인", 60_000, "음료"),
-    CHAMPAGNE("샴페인", 25_000, "음료");
+    MUSHROOM_SOUP("양송이수프", 6_000, MenuCategory.APPETIZER),
+    TAPAS("타파스", 5_500, MenuCategory.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8_000, MenuCategory.APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55_000, MenuCategory.MAIN),
+    BBQ_RIBS("바비큐립", 54_000, MenuCategory.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35_000, MenuCategory.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000, MenuCategory.MAIN),
+    CHOCO_CAKE("초코케이크", 15_000, MenuCategory.DESSERT),
+    ICECREAM("아이스크림", 5_000, MenuCategory.DESSERT),
+    ZERO_COKE("제로콜라", 3_000, MenuCategory.DRINK),
+    RED_WINE("레드와인", 60_000, MenuCategory.DRINK),
+    CHAMPAGNE("샴페인", 25_000, MenuCategory.DRINK);
 
     private final String name;
     private final int price;
     private final MenuCategory category;
 
-    private Menu(String name, int price, String categoryName) {
+    private Menu(String name, int price, MenuCategory category) {
         this.name = name;
         this.price = price;
-        this.category = MenuCategory.nameOf(categoryName);
+        this.category = category;
     }
 
     public String getName() {
@@ -33,7 +33,7 @@ public enum Menu {
     }
 
     public MenuCategory getCategory() {
-        return this.category;
+        return category;
     }
 
     public static Menu nameOf(String name) {
