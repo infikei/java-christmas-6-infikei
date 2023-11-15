a8d32d4e908f99f900f539c1dbe0f2e94a02556c
Infikei <hckhong@naver.com>
2023-11-15 19:52:25 +0900

    test(DateTest): 테스트 코드 리팩토링

--- src/test/java/christmas/model/DateTest.java
+++ src/test/java/christmas/model/DateTest.java
@@ -6,31 +6,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DateTest {
+    private void createDateAssertThatThrownBy(int date) {
+        assertThatThrownBy(() -> new Date(date))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void getDateAssertThat(int date, int expected) {
+        assertThat(new Date(date).getDate()).isEqualTo(expected);
+    }
+
     @Test
     void 날짜가_0인_경우_예외_발생() {
-        assertThatThrownBy(() -> new Date(0))
-                .isInstanceOf(IllegalArgumentException.class);
+        createDateAssertThatThrownBy(0);
     }
 
     @Test
     void 날짜가_음수인_경우_예외_발생() {
-        assertThatThrownBy(() -> new Date(-1))
-                .isInstanceOf(IllegalArgumentException.class);
+        createDateAssertThatThrownBy(-1);
     }
 
     @Test
     void 날짜가_32인_경우_예외_발생() {
-        assertThatThrownBy(() -> new Date(32))
-                .isInstanceOf(IllegalArgumentException.class);
+        createDateAssertThatThrownBy(32);
     }
 
     @Test
     void 날짜가_1인_경우_결과_확인() {
-        assertThat(new Date(1).getDate()).isEqualTo(1);
+        getDateAssertThat(1, 1);
     }
 
     @Test
     void 날짜가_31인_경우_결과_확인() {
-        assertThat(new Date(31).getDate()).isEqualTo(31);
+        getDateAssertThat(31, 31);
     }
 }
